29f03e1f28e046eafc738a175319bb7ad80748e0
mike <mike@lcd-outlet.com>
2021-03-28 13:03:50 +0000

    Walk_a_directory Recursively

--- file_utils.c
+++ file_utils.c
@@ -10,6 +10,7 @@
 #include        <string.h>
 #include	<stdlib.h>
 #include <stdio.h>
+#include "enum.h"
 
 //rosetta code
 char *ReadFile(char *filename)
@@ -52,24 +53,9 @@ char *ReadFile(char *filename)
 }
 
 
-enum {
-	LOOKUP = 0, /* default - looking rather than defining. */
-	WALK_OK = 0,
-	WALK_BADPATTERN,
-	WALK_NAMETOOLONG,
-	WALK_BADIO,
-};
-
-#define WS_NONE		0
-#define WS_RECURSIVE	(1 << 0)
-#define WS_DEFAULT	WS_RECURSIVE
-#define WS_FOLLOWLINK	(1 << 1)	/* follow symlinks */
-#define WS_DOTFILES	(1 << 2)	/* per unix convention, .file is hidden */
-#define WS_MATCHDIRS	(1 << 3)	/* if pattern is used on dir names too */
 
 
-
-
+//https://rosettacode.org/wiki/Walk_a_directory/Recursively#C
 int walk_recur(char *dname, regex_t *reg, int spec)
 {
 	struct dirent *dent;

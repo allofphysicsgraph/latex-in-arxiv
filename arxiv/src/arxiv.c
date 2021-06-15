fb7d75c8ad937bf85026f2d360632dfe9a08a4ff
mike <mike@mathrules.us>
2021-06-15 11:54:09 +0000

    add web page

--- arxiv/src/arxiv.c
+++ arxiv/src/arxiv.c
@@ -1,11 +1,16 @@
 #include <kore/kore.h>
 #include <kore/http.h>
+#include "assets.h"
 
 int		page(struct http_request *);
 
 int
 page(struct http_request *req)
 {
-	http_response(req, 200, NULL, 0);
+    
+	http_response_header(req, "content-type", "text/html");
+	http_response(req, 200, asset_index_html, asset_len_index_html);
 	return (KORE_RESULT_OK);
+
+
 }

9ce050a1ab229d1ce495b77d71a8f67c2b40a229
mike <mike@mathrules.us>
2021-07-18 16:30:19 +0000

    replace trie with simple truct

--- words.h
+++ words.h
@@ -2,5 +2,8 @@ int add_word(int type, char *word);
 int lookup_word(char *word,int print);
 int update_word(char *word);
 int lookup_word_by_word_type(int type,int print);
+int vocabulary_add_word(char *word);
+int vocabulary_lookup(char *word);
+int read_vocabulary_file();
 extern int lineCount;
 extern int offset;

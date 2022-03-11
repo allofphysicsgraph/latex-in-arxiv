588a51a65d375c948127e74066de2a48bbbd2946
mike <mike@mathrules.us>
2022-03-11 04:02:39 +0000

    simple version of resub

---
+++ basic_resub.c
@@ -0,0 +1,52 @@
+#include <errno.h>
+#include <math.h>  
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+#define MAX_FILE_SIZE 10000
+void resub(char *buffer, char *pattern,char* replacement); 
+char results[1024];
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	memset(results,'\0',1024);
+	resub("hello this is a pattern:       test:    again",":\\s+",": ");
+	return EXIT_SUCCESS;
+}		/* ----------  end of function main  ---------- */
+
+
+
+void resub(char *buffer, char *pattern,char* replacement) {
+  int current_offset = 0;
+  char current_buffer[MAX_FILE_SIZE];
+  regex_t preg;
+  regmatch_t substmatch[1];
+  regcomp(&preg, pattern, REG_EXTENDED); // this assumes matching <
+  current_offset = 0;
+  memset(current_buffer, '\0', MAX_FILE_SIZE);
+  strcpy(current_buffer, buffer);
+  while (current_offset < strlen(buffer)) {
+    if (regexec(&preg, current_buffer, 1, substmatch, 0) == 0) {
+      memset(current_buffer, '\0', MAX_FILE_SIZE);
+      strncpy(current_buffer, &buffer[current_offset],substmatch[0].rm_so);
+      strcat(current_buffer, &buffer[current_offset + substmatch[0].rm_eo]);
+      strncat(results,current_buffer,substmatch[0].rm_so);
+      strcat(results,replacement);
+      current_offset += substmatch[0].rm_eo;
+      strcpy(current_buffer, &buffer[current_offset]);
+    } else {
+      strcat(results,current_buffer);
+      break;
+    }
+  }
+  printf("%s\n", results);
+  regfree(&preg);
+}
+

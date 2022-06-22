0550d5ca52abf7b437e9163276c6d57e6b1d6874
mike <mike@mathrules.us>
2022-06-22 19:31:48 +0000

    removed unused files

--- old/process_sample_data.sh
+++ old/process_sample_data.sh
@@ -14,9 +14,9 @@ cd 2003/
 while read f;do ./a.out $f;done < <(ls|grep 03)
 while read f;do cat $f|newcommand > "$f"_newcmd_expanded;done < <(ls|grep ^03|grep -v '\.'|grep -v newcmd)
 
-#run with caution, this may consume all free memory 
-#ls|grep ^03|grep -v a.out|grep -v '\.' |xargs -i -P0 pdflatex "{}" 
-#cat newcommand_expansion_verified |xargs -i echo 2003/"{}" |xargs -i cp "{}" SAMPLE_DATA/
+#may consume all free memory 
+	#ls|grep ^03|grep -v a.out|grep -v '\.' |xargs -i -P0 pdflatex "{}" 
+	#cat newcommand_expansion_verified |xargs -i echo 2003/"{}" |xargs -i cp "{}" SAMPLE_DATA/
 
 #modify path in sed accordingly to cleanup the output
 #ls|grep pdf|xargs -i ssdeep "{}" |sed -r 's/\/home\/user\/latex-in-arxiv\/2003\///g'|tee ../output

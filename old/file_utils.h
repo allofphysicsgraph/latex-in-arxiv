f93d298ba91ea7599f01bc057239f3f1b9067736
mike <mike@mathrules.us>
2022-06-22 21:17:58 +0000

    removed duplicate files

--- old/file_utils.h
+++
@@ -1,5 +0,0 @@
-#include <regex.h>
-char *ReadFile(char *filename);
-int walk_recur(char *dname, regex_t *reg, int spec,char * array[],int array_index);
-int walk_dir(char *dname, char *pattern, int spec, char * array[], int array_index);
-

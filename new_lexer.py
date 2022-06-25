1aedd70bf405bd19949627603e009149edaf6b81
mike <mike@mathrules.us>
2022-06-25 01:14:51 +0000

    simple user defined token def match

--- new_lexer.py
+++ new_lexer.py
@@ -1,4 +1,12 @@
 from sys import argv
+from nltk.tokenize import mwe
+from time import sleep
+import re
+import nltk
+
+
+def add_new_token(string):
+    tokenizer.add_mwe(r"{}".format(string))
 
 
 def read_file(path, f_name):
@@ -23,7 +31,7 @@ def balanced(start, s, left_symbol=r"{", right_symbol=r"}"):
             # print(match)
             left_symbol = f"""\\begin{{{match}}}"""
             right_symbol = f"""\\end{{{match}}}"""
-    print(left_symbol)
+    # print(left_symbol)
     if left_symbol != right_symbol:
         current_offset = 0
         balanced = 0
@@ -57,6 +65,7 @@ def math_ctrl_seq(data):
     # multiline is not matched,    #\[.*?\] is also not matched
     # likely to be other issues.
     import re
+    from time import sleep
 
     lst_errors = [x for x in re.findall(r"\$.*?\$", data) if x.count("$") % 2 != 0]
     print("error count:{}\n".format(len(lst_errors)))
@@ -76,20 +85,74 @@ def word_lst(data):
     return matched_words, unmatched_words
 
 
+def comments(data):
+    import re
+
+    lst = re.findall("%.*", data)
+    return lst
+
+
+def symbol_definitions(sentence):
+    lst = []
+    match = re.findall(r"where \$[a-zA-Z]\$ is.*?(?:\$|$)", sentence, re.DOTALL)
+    if match:
+        lst.extend(match[0])
+    match = re.findall(r"and \$[a-zA-Z]\$ is.*?(?:\$|$)", sentence, re.DOTALL)
+    if match:
+        # if matched here, the sentence should be split and checked for a previous definition of another symbol.
+        lst.extend(match)
+
+    match = re.findall(r"\$[a-zA-Z]\$-function", sentence)
+    if match:
+        # if matched here, the sentence should be split and checked for a previous definition of another symbol.
+        lst.extend(match)
+    return lst
+
+
 if __name__ == "__main__":
     from collections import defaultdict
     from pudb import set_trace
 
+    tokenizer = mwe.MWETokenizer(separator="")
+    latex_tokens = [x.strip() for x in read_file(".", "latex_vocab").splitlines()]
+    for token in latex_tokens:
+        add_new_token(token)
     dct = defaultdict(list)
     file_data = read_file(".", argv[1])
-    #TODO auto enumerate ctrl sequences other than begin,frac and the like something like '(\\[a-z]+){' might work
     dct["cite"].extend(balanced(r"\\cite", file_data))
+    dct["date"].extend(balanced(r"\\date", file_data))
     dct["packages"].extend(balanced(r"\\usepackage", file_data))
     dct["title"].extend(balanced(r"\\title", file_data))
     dct["authors"].extend(balanced(r"\\author", file_data))
     dct["affiliations"].extend(balanced(r"\\affiliation", file_data))
     dct["abstract"].extend(balanced(r"\\begin{abstract}", file_data))
+    dct["subequations"].extend(balanced(r"\\begin{subequations}", file_data))
     dct["equations"].extend(balanced(r"\\begin{equation}", file_data))
+    dct["equationarray"].extend(balanced(r"\\begin{eqnarray}", file_data))
+    dct["ref"].extend(balanced(r"\\ref", file_data))
+    dct["pacs"].extend(balanced(r"\\pacs", file_data))
+    dct["keywords"].extend(balanced(r"\\keywords", file_data))
+    dct["label"].extend(balanced(r"\\label", file_data))
+    dct["section"].extend(balanced(r"\\section", file_data))
     dct["math_ctrl_seq"].extend(math_ctrl_seq(file_data))
-    # print(dct['math_ctrl_seq'])
-    print(word_lst(file_data))
+    dct["comments"].extend(comments(file_data))
+    # print(word_lst(file_data))
+    # print(dct['ref'])
+    # set_trace()
+    for token in dct["math_ctrl_seq"]:
+        if len(token) == 1:
+            if re.findall("[a-zA-z]", token):
+                if token.strip() not in dct["user_defined_symbols"]:
+                    dct["user_defined_symbols"].append(token.strip())
+                    # print(token)
+        # print(token,tokenizer.tokenize(r'{}'.format(token)))
+        # sleep(1)
+
+    dct["sentences"] = nltk.sent_tokenize(file_data)
+    for ix, sent in enumerate(dct["sentences"]):
+        print("sent:", ix, sent)
+        # set_trace()
+        dct["user_defined_symbols_definition"].extend(symbol_definitions(sent))
+        # sleep(2)
+        print("\n")
+    # set_trace()

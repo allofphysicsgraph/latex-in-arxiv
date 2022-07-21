002cea5614766829d2517e83c254dcc859cab7d8
mike <mike@mathrules.us>
2022-07-21 19:07:48 +0000

    verifying counts

---
+++ run_verify_counts.sh
@@ -0,0 +1,10 @@
+grep \\\\author -irl 2003|xargs -i bash verify_counts.sh \\cite "{}" |grep -v ok
+
+#example output on mismatch for \\cite
+#0,1,2003/0301062.tex
+#94,99,2003/0301080.tex
+#9,60,2003/0302147.tex
+#23,25,2003/0303116.tex
+#0,1,2003/0304269.tex
+
+

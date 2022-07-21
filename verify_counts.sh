3b5a959af7c7056890cf01f3453c3aa16d838d02
mike <mike@mathrules.us>
2022-07-21 21:20:30 +0000

    testing counts

--- verify_counts.sh
+++ verify_counts.sh
@@ -1,8 +1,9 @@
 python_count="$(./verify_counts.py "$1" "$2")"
 flex_count="$(./a.out $2)"
+control_sequence="$(echo $1|tr -d '\\')"
 if [[ $python_count != $flex_count ]];
 then 
-   echo $python_count,$flex_count,$2
+   echo $python_count,$flex_count,$2,$control_sequence
 else
 	echo ok
 fi

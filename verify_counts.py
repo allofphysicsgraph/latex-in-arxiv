25581e06a8b41a147c9137071c19fe3c4ca2da77
mike <mike@mathrules.us>
2022-07-21 18:49:46 +0000

    verify counts

---
+++ verify_counts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from sys import argv
+import re
+
+def read_file(f_name):
+    with open(f"{f_name}", "r", encoding="ISO-8859-1") as f:
+        data = f.read()
+    clean = [x for x in data.splitlines() if not re.findall(r"^\\def|^\\newcommand", x)]
+    data = "\n".join(clean)
+    return data
+
+
+data = read_file(argv[2])
+print(len(re.findall(r'{}'.format(re.escape(argv[1])),data)))

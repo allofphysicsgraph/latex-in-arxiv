a58a4d091031cf4c0009adb4d43375a3be26f7cd
mike <mike@mathrules.us>
2022-07-22 18:17:31 +0000

    pacs

--- tools/find_duplicate_files_in_repo.sh
+++ tools/find_duplicate_files_in_repo.sh
@@ -1,2 +1,2 @@
 find ../ -type f -exec sha256sum "{}" \; > /dev/shm/chksm_files ;
-#grep -f <( cat /dev/shm/chksm_files| cut -d ' ' -f1 |sort|uniq -c |sort -n|awk '$1 > 1'|awk '{print $2}') /dev/shm/chksm_files
+grep -f <( cat /dev/shm/chksm_files| cut -d ' ' -f1 |sort|uniq -c |sort -n|awk '$1 > 1'|awk '{print $2}') <(cat /dev/shm/chksm_files)

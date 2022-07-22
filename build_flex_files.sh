1d3b658901bfe1b7b83e92a049dc5ca9c94b4fb5
mike <mike@mathrules.us>
2022-07-22 17:57:38 +0000

    equations from 2003 ArXiV dataset

--- build_flex_files.sh
+++ build_flex_files.sh
@@ -1,4 +1,4 @@
 while read f;
 do flex -Cf "$f".l;
 	gcc -lfl lex.yy.c -o "$f".out
-done < <(find . -maxdepth 1 -type f -name "*.l"|cut -d '/' -f2 |cut -d '.' -f1)
+done < <(find . -maxdepth 1 -type f -name "*.l"|cut -d '/' -f2 |cut -d '.' -f1|grep -v bloom|grep -v menu)

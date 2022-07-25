83aad7f86b95481b50b6bdc322456e4265560c89
mike <mike@mathrules.us>
2022-07-25 02:25:44 +0000

    removed some duplicate training data sentences that appears multiple times in a file

--- tools/find_duplicate_files_in_repo.sh
+++ tools/find_duplicate_files_in_repo.sh
@@ -1,2 +1,5 @@
 find ../ -type f -exec sha256sum "{}" \; > /dev/shm/chksm_files ;
-#./a.out hello |grep -P "\t" |tr -d '\t .' |cut -d '/' -f4|xargs -i find x/latex-in-arxiv/data/training_data/ -type f -name "{}" -exec rm x/latex-in-arxiv/data/training_data/"{}" \;
+
+#removing duplicates in training data
+#./a.out hello |grep -P "\t" -C 2|grep train -C 2|grep -P '\t'|sed -r 's/^\s+//g'|sed -r 's/^\.\///g'|xargs -i rm ../"{}"
+

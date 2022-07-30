7dc06b64e2ccff6c3486277d42c7b7eecc1ce98b
mike <mike@mathrules.us>
2022-07-30 17:48:14 +0000

    scripts to verifying the counts between flex and python

--- verify_counts.sh
+++ verify_counts.sh
@@ -1,7 +1,7 @@
 function compare_counts { 
 flex_file_name=$(echo "$1".out)
 python_count="$(./verify_counts.py "$1" "$2")"
-flex_count="$(./$flex_file_name $2|awk 'length($0) > 0 '|grep "$1" | wc -l)"
+flex_count="$(./$flex_file_name $2 --count)"
 control_sequence="$(echo $2|tr -d '\\')"
 if [[ $python_count != $flex_count ]];
 then 
@@ -18,9 +18,11 @@ do
 	if [[ -f "$f".out ]];
 	then
 		echo "$f"
-		compare_counts $f $2
+		compare_counts $f $1
 	else
 		echo "$f:missing"
 	fi
 
-done < <( grep -oP '\\[a-z]+{' "$2" |sort|uniq -c |sort -n |awk '{print $2}'|tr -d '{\\'|grep -vPx 'end|omicron|caption|includegraphics|scalebox|mathbf|begin|rho|mu|nu|tilde|beta|alpha|hat|bar')
+done < <( grep -oP '\\[a-z]+{' "$1" |sort|uniq -c |sort -n |awk '{print $2}'|tr -d '{\\'|grep -vPx 'frac|end|omicron|caption|includegraphics|scalebox|mathbf|begin|rho|mu|nu|tilde|beta|alpha|hat|bar|vspace|textsf|center|mathbbm|noindent|phi|partial')
+#|tee >> missing
+#cat missing | grep missing|cut -d ':' -f1|tr '\n' '|'|sed 's/|$//g'

f6ade005c686dcda648d1ecc5a1ddaf967935cf4
mike <mike@mathrules.us>
2022-07-30 04:54:28 +0000

    fix if condition for balanced

--- run_all_flex_scripts.sh
+++ run_all_flex_scripts.sh
@@ -1,2 +1,5 @@
 /bin/bash build_flex_files.sh
-ls|grep out$|xargs -i /bin/bash run_flex_script.sh "{}" 
+#fractions should be ran on it's own
+#a few errors to resolve with fractions, but works pretty well
+
+ls|grep out$|grep -v fraction| xargs -i /bin/bash run_flex_script.sh "{}" 

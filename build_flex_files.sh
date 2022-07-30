ef60581292a66bfecf0722d8584b87ae1dd3a2d3
mike <mike@mathrules.us>
2022-07-30 04:54:28 +0000

    fix if condition for balanced

--- build_flex_files.sh
+++ build_flex_files.sh
@@ -1,4 +1,5 @@
 while read f;
 do flex -Cf "$f".l;
+	echo $f
 	gcc -lfl lex.yy.c -o "$f".out
 done < <(find . -maxdepth 1 -type f -name "*.l"|cut -d '/' -f2 |cut -d '.' -f1|grep -v bloom|grep -v menu)

cdc4b4d599afefb634a68b5d82d3c402d151cd82
mike <mike@mathrules.us>
2022-08-06 05:29:59 +0000

    output to csv

--- flex_files/REGENERATE_ALL_FLEX_SCRIPTS.sh
+++ flex_files/REGENERATE_ALL_FLEX_SCRIPTS.sh
@@ -5,19 +5,6 @@ fi
 find . -maxdepth 1 -name "*.l" -exec rsync -axr  "{}" flex_scripts_backup/ \; 
 
 arr=(address affiliation author bibitem bibliography cite date footnote keywords label pacs ref section setcounter sqrt thanks title usepackage)
-# pop ARRAY -- pop the last item on ARRAY and output it
-
-pop() {
-  local var=${1:?'Missing array name'}
-  local x ;   eval "x=\${#$var[*]}"
-  if [[ $x > 0 ]]; then
-    local val ; eval "val=\"\${$var[$((--x))]}\""
-    unset $var[$x]
-  else
-    echo 1>&2 "No items in $var" ; exit 1
-  fi
-  echo "$val"
-}
 
 
 for ((i=0;i<${#arr[*]};i++))

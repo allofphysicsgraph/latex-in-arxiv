b8be3aa8f0c2600ffdee6929d2dee006a856a2d8
mike <mike@mathrules.us>
2022-08-13 15:13:13 +0000

    Testing Status

--- flex_files/verify_counts.sh
+++ flex_files/verify_counts.sh
@@ -3,12 +3,11 @@ flex_file_name=$(echo "$1".out)
 python_count="$(./verify_counts.py "$1" "$2")"
 flex_count="$(./$flex_file_name $2 --count)"
 control_sequence="$(echo $2|tr -d '\\')"
-echo $control_sequence
 if [[ $python_count != $flex_count ]];
 then 
-   echo  $python_count,$flex_count,$2,$control_sequence
+   echo  $2,$python_count,$flex_count,$1,FAIL
 else
-	echo "    count matches"
+   echo  $2,count matches,$1,OK
 fi
  }
 
@@ -18,10 +17,9 @@ while read f;
 do 
 	if [[ -f "$f".out ]];
 	then
-		echo "$f"
 		compare_counts $f $1
 	else
-		echo "$f:missing"
+		echo "$1,$f:missing,FAIL"
 	fi
 
 done < <( grep -oP '\\[a-z]+{' "$1" |sort|uniq -c |sort -n |awk '{print $2}'|tr -d '{\\'|grep -vPx 'frac|end|omicron|caption|includegraphics|scalebox|mathbf|begin|rho|mu|nu|tilde|beta|alpha|hat|bar|vspace|textsf|center|mathbbm|noindent|phi|partial')

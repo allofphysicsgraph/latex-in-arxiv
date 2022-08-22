fc096cc8881e8a820b4c9b561c1bf1b7ac6bfd5d
mike <mike@mathrules.us>
2022-08-22 04:02:46 +0000

    merged scanners into main.c

--- flex_files/frac/globals.h
+++ flex_files/frac/globals.h
@@ -2,4 +2,6 @@ void resub(char *buffer, char *pattern,char* replacement);
 extern char results[1024];
 extern char filename[256];
 extern char results_filename[256];
-
+extern int balanced;
+extern int count;
+extern int print_count;

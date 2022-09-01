87f4c6b60ee437350bbc08fa096c2495cda4988e
mike <mike@mathrules.us>
2022-09-01 20:11:21 +0000

    find words not matched by vocab list

--- test.py
+++ test.py
@@ -2,20 +2,16 @@ from common import read_file
 import ahocorasick
 import nltk
 
-file = read_file('.','english.vocab')
+nltk.download("punkt")
+
+file = read_file(".", "english.vocab")
 file_data = file.splitlines()
 
 A = ahocorasick.Automaton(ahocorasick.STORE_INTS)
 for word in file_data:
     A.add_word(word)
-sound = read_file('.','sound1.tex')
-sents = nltk.sent_tokenize(sound)
-nltk.download('punkt')
-sound
-nltk.sent_tokenize(sound)
-sents = nltk.sent_tokenize(sound)
-nltk.word_tokenize(sound)
-words = nltk.word_tokenize(sound)
+
+sound = read_file(".", "sound1.tex")
 s = set()
 for word in nltk.word_tokenize(sound):
     try:

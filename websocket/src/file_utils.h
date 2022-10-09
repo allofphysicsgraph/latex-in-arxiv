ea5a86c0196934fb9cd2285b1dcd8056073086d2
mike <mike@mathrules.us>
2022-10-09 09:44:09 +0000

    render file index as clickable links in frontend.html

--- websocket/src/file_utils.h
+++ websocket/src/file_utils.h
@@ -1,7 +1,8 @@
 #include <regex.h>
+#include <hiredis.h>
 
 char *ReadFile(char *filename);
 int walk_recur(char *dname, regex_t *reg, int spec,char * array[],int array_index);
 int walk_dir(char *dname, char *pattern, int spec, char * array[], int array_index);
 void file_search(char* dir_path);
-redisContext *c = Conn();
+redisContext *Conn(void);

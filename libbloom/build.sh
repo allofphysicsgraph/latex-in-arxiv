8c7d92cb70f73004b839e019ba2cb6c7320f26d4
mike <mike@mathrules.us>
2022-12-14 22:14:52 +0000

    bloom_filter

--- libbloom/build.sh
+++ libbloom/build.sh
@@ -1,2 +1,4 @@
-gcc bloom_filter_test.c bloom.c  -Lbuild/libbloom.so murmur2/MurmurHash2.c -lm
 
+gcc bloom_filter_test.c bloom.c  -Lbuild/libbloom.so murmur2/MurmurHash2.c -lm
+cp ../english.vocab .
+echo './a.out search_term'

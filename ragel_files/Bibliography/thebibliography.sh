26fb741f2fc23a06456c0a2e9e3e9b0aeea773ba
mike <mike@mathrules.us>
2023-05-07 22:56:02 +0000

    10k or conditions handled flawlessly in Ragel

--- ragel_files/Bibliography/thebibliography.sh
+++ ragel_files/Bibliography/thebibliography.sh
@@ -1,2 +1,3 @@
+#for i in `seq 1 10 10000`;do sed -n $i','$(($i+10))p names|tr '\n' '|' ;echo "=> \{ printf\("token: %s\n", std::string\(ts, te\).c_str\(\)\); \};;";echo ;echo ;done
 grep "begin{thebibliography}|end{thebibliography}" -P "$1" -n|cut -d ':' -f1|tr '\n' ','|sed -r 's/,$//g'|xargs -i echo sed -n "{}"p "$1"
 

ba3d13a5300f8d448947692e474d442d3ac7b41f
mike <mike@mathrules.us>
2023-05-16 14:39:36 +0000

    db setup

--- db_init.sh
+++ db_init.sh
@@ -2,9 +2,16 @@ sudo -u postgres -H -- psql -c "create user latexinarxiv with password '3e9f9148
 sudo -u postgres -H -- psql -c 'create database latexinarxiv with owner latexinarxiv;' 
 cp db_init.sql /dev/shm/
 cp db_schema /dev/shm/
-cp data/equation.csv
-
+cp data/equation.csv /dev/shm/
+cp data/title.csv /dev/shm/
+cp data/author.csv /dev/shm/
+cp data/cite.csv /dev/shm/
+cp data/section.csv /dev/shm/
 sudo -u postgres -H -- psql -d latexinarxiv -f /dev/shm/db_init.sql 
 sudo -u postgres -H -- psql -d latexinarxiv -f /dev/shm/db_schema 
-sudo -u postgres -H -- psql -c "copy equation from '/dev/shm/equation.csv' CSV;" 
+sudo -u postgres -H -- psql -d latexinarxiv -c "copy equation from '/dev/shm/equation.csv' CSV;" 
+sudo -u postgres -H -- psql -d latexinarxiv -c "copy title from '/dev/shm/title.csv' CSV;" 
+sudo -u postgres -H -- psql -d latexinarxiv -c "copy author from '/dev/shm/author.csv' CSV;" 
+sudo -u postgres -H -- psql -d latexinarxiv -c "copy section from '/dev/shm/section.csv' CSV;" 
+sudo -u postgres -H -- psql -d latexinarxiv -c "copy cite from '/dev/shm/cite.csv' CSV;" 
 

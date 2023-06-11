c973097411293b38e4a7738c3f01f561c2fe9ee4
mike <mike@mathrules.us>
2023-06-11 03:54:39 +0000

    reorganized files

--- db_init.sql
+++
@@ -1,8 +0,0 @@
-
-create schema api;
-alter schema api owner to latexinarxiv;
-create role web_anon nologin;
-grant usage on schema api to web_anon;
-create role authenticator noinherit login password 'mysecretpassword';
-grant web_anon to authenticator;
-

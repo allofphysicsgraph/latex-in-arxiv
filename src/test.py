fb9c686226a959f7dd72ef0d74d160543138146b
mike <mike@mathrules.us>
2023-06-24 08:01:00 +0000

    basic template for begin end patterns

---
+++ src/test.py
@@ -0,0 +1,10 @@
+
+from ragel_begin_end import RagelBeginEnd
+
+x = RagelBeginEnd()
+
+with open('/home/user/latex-in-arxiv/sound1.tex','r') as f:
+    data = f.read()
+
+
+x.exposed_get_abstract(data=data)

eacc8586ab7b614df359518d2c10cb080e316e94
mike <mike@mathrules.us>
2023-11-24 18:38:37 +0000

    cleanup repo

--- utils/find_duplicate_files_in_repo.sh
+++
@@ -1,5 +0,0 @@
-find ../ -type f -exec sha256sum "{}" \; > /dev/shm/chksm_files ;
-
-#removing duplicates in training data
-#./a.out hello |grep -P "\t" -C 2|grep train -C 2|grep -P '\t'|sed -r 's/^\s+//g'|sed -r 's/^\.\///g'|xargs -i rm ../"{}"
-
--- utils/generate_flex_files_wbegin.sh
+++
@@ -1,8 +0,0 @@
-while read f;
-	do 
-		echo cp center.l "$f".l; 
-		upper=$(echo $f|tr  'a-z' 'A-Z');
-		echo sed -i \'s/CENTER/$upper/g\' "$f".l;
-		echo sed -i \'s/center/$f/g\' "$f".l ;
-	done < <(cat BEGINS.sh)
-

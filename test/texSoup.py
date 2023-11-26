b1b533e9f1e745243f7e0cca0a1d42a6b283be60
mike <mike@mathrules.us>
2023-11-26 17:48:13 +0000

    working on benchmarks

---
+++ test/texSoup.py
@@ -0,0 +1,9 @@
+from TexSoup import TexSoup
+from time import time
+start=time()
+with open('/home/user/latex-in-arxiv/sound1.tex','r') as f:
+    data = f.read()
+stop=time()-start
+print(stop)
+soup=TexSoup(data)
+print(len(soup.find_all('$')))

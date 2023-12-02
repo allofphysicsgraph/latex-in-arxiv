26c8ed73ccc74209dd747ab80c5f42ba1016019d
mike <mike@mathrules.us>
2023-12-02 03:32:29 +0000

    print tokens/ids

--- src/postings_list/query/example.c
+++ src/postings_list/query/example.c
@@ -3,12 +3,7 @@
 #include <stdio.h>  /* printf */
 #include <stdlib.h> /* atoi, malloc */
 #include <string.h> /* strcpy */
-
-struct my_struct {
-  XXH64_hash_t id; /* key */
-  char token[21];
-  UT_hash_handle hh; /* makes this structure hashable */
-};
+#include "globals.h"
 
 struct my_struct *tokens = NULL;
 
@@ -76,66 +71,3 @@ const char *getl(const char *prompt) {
   return buf;
 }
 
-int main() {
-  XXH64_hash_t id = XXH64("hello", 5, 0);
-  int running = 1;
-  struct my_struct *s;
-  int temp;
-
-  while (running) {
-    printf(" 1. add token\n");
-    printf(" 2. add or rename token by id\n");
-    printf(" 5. delete all tokens\n");
-    printf(" 6. sort items by name\n");
-    printf(" 7. sort items by id\n");
-    printf(" 8. print tokens\n");
-    printf(" 9. count tokens\n");
-    printf("10. quit\n");
-    switch (atoi(getl("Command"))) {
-    case 1:
-      static char buf[21];
-      memset(buf, '\0', 21);
-      strncpy(buf, getl("Name (20 char max"), 21);
-      add_token(XXH64(buf, strlen(buf), 0), buf);
-      break;
-    case 2:
-      temp = atoi(getl("ID"));
-      add_token(temp, getl("Name (20 char max)"));
-      break;
-    case 3:
-      // s = find_token(atoi(getl("ID to find")));
-      // printf("token: %s\n", s ? s->token : "unknown");
-      break;
-    case 4:
-      s = find_token(atoi(getl("ID to delete")));
-      if (s) {
-        delete_token(s);
-      } else {
-        printf("id unknown\n");
-      }
-      break;
-    case 5:
-      delete_all();
-      break;
-    case 6:
-      HASH_SORT(tokens, by_token);
-      break;
-    case 7:
-      HASH_SORT(tokens, by_id);
-      break;
-    case 8:
-      print_tokens();
-      break;
-    case 9:
-      temp = HASH_COUNT(tokens);
-      printf("there are %d tokens\n", temp);
-      break;
-    case 10:
-      running = 0;
-      break;
-    }
-  }
-
-  delete_all(); /* free any structures */
-  return 0;
-}

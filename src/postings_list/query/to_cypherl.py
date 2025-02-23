d20c6c452a077f18c3baa78dccca5aa84720a619
mike <mike@mathrules>
2025-02-23 00:55:08 +0000

    blackened

--- src/postings_list/query/to_cypherl.py
+++ src/postings_list/query/to_cypherl.py
@@ -8,10 +8,10 @@ import re
 with open("offsets", "r") as f:
     offsets = re.split("(\.\/.*?tex)\n", f.read())
 
+
 offsets = [x for x in offsets if x.strip()]
 filenames = offsets[::2]
 rows = offsets[1::2]
-
 
 files_to_offsets = defaultdict(pd.DataFrame)
 for ix, fileData in enumerate(rows):
@@ -45,17 +45,16 @@ for k, v in files_to_offsets.items():
         df = pd.concat([df, v])
         sm += len(v)
 
-#print(tf_idf_df)
 tf_idf_df.columns = ["id", "count", "doc_count", "tf_idf", "token"]
 zf = pd.merge(df, tf_idf_df, left_on="id", right_on="id")
+
 zf["length"] = zf["length"].apply(lambda x: int(x))
 zf = zf[zf["length"] > 1]
-zf = zf[zf.token.apply(lambda x: True if re.findall("\$.*?\$", x) else False)]
-zf = zf[zf.token.apply(lambda x: False if re.findall("abstract", x) else True)]
-print(len(zf))
-print(zf.head())
-#from pudb import set_trace
-
+# zf = zf[zf.token.apply(lambda x: True if re.findall("\$.*?\$", x) else False)]
+# zf = zf[zf.token.apply(lambda x: False if re.findall("abstract", x) else True)]
+# print(len(zf))
+# print(zf.head())
+# exit()
 # file_paths = set(zf.filename.tolist())
 X = zf.itertuples()
 # set_trace()
@@ -81,7 +80,7 @@ with open("draft.cypherl", "a+") as f_out:
                 f", (t:Token {{length:{length},token_count:{token_count},docs_count:{docs_count},tf_idf:{tf_idf},hash:'{token_id}',offset:{offset}}})"
             )
             # f_out.write("\n")
-            f_out.write(" Create (f)-[r:file_has_token]->(t);")
+            f_out.write(" Create (f)-[r:file_has_equation]->(t);")
             # f_out.write("\n")
             # f_out.write("RETURN f, t ,t;")
             f_out.write("\n")

2d7602fe03ca83c10e4a5b74a2c2134cf9f1bf39
mike <mike@mathrules.us>
2025-03-16 21:16:40 +0000

    blackened

--- src/postings_list/query/get_sentences.py
+++ src/postings_list/query/get_sentences.py
@@ -1,25 +1,28 @@
 from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 import nltk
 from sys import argv
-import re 
-#depends on nltk 3.8 which has a CVE
-#also depends on 285M file punkt_html_trainer. 
-#send an email or post an issue if you want a copy of the pickled punkt model. 
-#I find that it is performs better than the default sentence punkt english model. 
+import re
 
-punkt_trainer = nltk.data.load("punkt_html_trainer",format='pickle')
+# depends on nltk 3.8 which has a CVE
+# also depends on 285M file punkt_html_trainer.
+# send an email or post an issue if you want a copy of the pickled punkt model.
+# I find that it is performs better than the default sentence punkt english model.
+
+punkt_trainer = nltk.data.load("punkt_html_trainer", format="pickle")
 tok_cls = PunktSentenceTokenizer(punkt_trainer.get_params())
 from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
+
 tok_cls = PunktSentenceTokenizer(punkt_trainer.get_params())
-with open(argv[1],'r') as f:
+with open(argv[1], "r") as f:
     data = f.read()
-    re.sub(r'\\begin{thebibliography}.*?\\end{thebibliography}','',data,re.DOTALL)
+    re.sub(r"\\begin{thebibliography}.*?\\end{thebibliography}", "", data, re.DOTALL)
     print(data)
 
 tok_cls.sentences_from_text(data)
 sentences = tok_cls.sentences_from_text(data)
 from time import sleep
+
 for ix in range(len(sentences)):
     print(sentences[ix])
-    print('*'*50)
+    print("*" * 50)
     sleep(2)

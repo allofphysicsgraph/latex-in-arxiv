5ce25f1dbad994e4a2ea1cb1e2e49e447d385302
mike <mike@mathrules.us>
2025-05-16 15:00:09 +0000

    not used

--- old/libbloom/update_postings_list.sql
+++
@@ -1,8 +0,0 @@
-copy postings_list (filename,offset_idx,len,word) from '/dev/shm/postings.csv' CSV;
-
-update postings_list
-set cnt = x.count
-from (select count(word),filename,word from postings_list  group by filename,word ) x
-where postings_list.word =x.word
-and postings_list.filename = x.filename;
-

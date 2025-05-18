59206eedbd03565fd9c3f96e044eb5719d62a7f2
mike <mike@mathrules.us>
2025-05-18 19:28:05 +0000

    refactor

--- src/postings_list/query/globals.h
+++ src/postings_list/query/globals.h
@@ -1,28 +1,27 @@
 #include "uthash.h"
+#include <ftw.h>
 #include <stdio.h>  /* printf */
 #include <stdlib.h> /* atoi, malloc */
 #include <string.h> /* strcpy */
-#include <ftw.h>
 
 #define _XOPEN_SOURCE 500 // Required for nftw in some systems
+#include <errno.h>
 #include <ftw.h>
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
-#include <errno.h>
 
-#define MAX_FILE_PATH_LENGTH 128
-#define MAX_TOKEN_COUNT 2560
-#define MAX_TOKEN_LENGTH 2500
+#define MAX_TOKEN_LENGTH 2000
 
 int scanner(const char *in, int length);
+uint32_t murmur3_seeded_v2(uint32_t seed, const char *data, size_t len);
+//const uint32_t seed0 = 0x293ae76f;
 
 struct my_struct {
   uint32_t id;
   int count;
   int length;
-
   char current_file[256];
   int doc_count; /* number of docs that contains this token */
   float tf_idf;
@@ -30,8 +29,8 @@ struct my_struct {
   UT_hash_handle hh; /* makes this structure hashable */
 };
 
-void write_tf_idf();
+//void write_tf_idf();
 void add_token(uint32_t token_id, const char *token, int length,
                char filename[256]);
-void delete_all();
-void srt();
+//void delete_all();
+//void srt();
